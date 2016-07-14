3142447ff7bc4cbb914ca4520e7793b6bd42fb1d
prince-0203 <mc.prince.0203@gmail.com>
2016-07-14 20:49:00 +0900

    Fix #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,16 @@
 /* jshint esnext: true */
 const gulp = require("gulp");
 const sass = require('gulp-sass');
-const react = require('gulp-react');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
 const runSequence = require('run-sequence');
 const rimraf = require('rimraf');
 
 // Clean
 gulp.task('clean', function(cb) {
   rimraf('./dest', cb);
-});
-
-// Copy
-gulp.task('copy', function() {
-  return gulp.src('./js/*.js')
-    .pipe(gulp.dest('./dest/'));
 });
 
 // SASS
@@ -32,29 +24,16 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./dest/'));
 });
 
-// React.js
-gulp.task('react', function() {
-  return gulp.src('./js/*.jsx')
-    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-    .pipe(sourcemaps.init())
-    .pipe(react())
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dest/'));
-});
-
 // Watch
 gulp.task('watch', function() {
   gulp.watch('./sass/*.scss', ['sass']);
-  gulp.watch('./js/*.jsx', ['react']);
 });
 
 // Build
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    ['sass', 'react'],
-    'copy',
+    'sass',
     callback
   );
 });
